7cf745bd93742e4c6e0074e91bfe53131a34a64c
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-07-31 12:36:45 +0000

    Mon, Jul 31, 2023, 6:06 PM +05:30

--- prime/prime.c
+++ prime/prime.c
@@ -35,14 +35,14 @@ bool prime(int number)
     int count = 0;
     for (int i = 1; i <= number; i++)
     {
-        if ( number % i==0 )
+        if (number % i == 0)
         {
             count++;
         }
     }
 
-    if(count == 2)
-    return true;
+    if (count == 2)
+        return true;
     else
-    return false;
+        return false;
 }
