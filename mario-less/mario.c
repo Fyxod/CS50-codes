5b32a827a9b4f8311a5042cc9ddd3e19973f8051
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-08-01 09:54:17 +0000

    Tue, Aug 1, 2023, 3:24 PM +05:30

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,21 +4,21 @@
 int main(void)
 {
     int h;
-   do
-   {
-    h = get_int("height: ");
-   }
-   while(h > 8 || h < 1);
-
-   for (int i = (h - 1); i >= 0; i--)
-   {
-    for (int j = 0; j < h; j++)
+    do
     {
-        if (j >= i)
-        printf ("#");
-        else
-        printf(" ");
+        h = get_int("height: ");
     }
-    printf("\n");
-   }
+    while (h > 8 || h < 1);
+
+    for (int i = (h - 1); i >= 0; i--)
+    {
+        for (int j = 0; j < h; j++)
+        {
+            if (j >= i)
+                printf("#");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
 }
