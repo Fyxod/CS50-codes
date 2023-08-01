8df96bc0bce8a39cbc0084ec32ee06dae153d77a
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-08-01 10:03:14 +0000

    Tue, Aug 1, 2023, 3:33 PM +05:30

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int h;
+    do
+    {
+        h = get_int("height: ");
+    }
+    while (h > 8 || h < 1);
+
+    for (int i = (h - 1); i >= 0; i--)
+    {
+        for (int j = 0; j < h; j++)
+        {
+            if (j >= i)
+                printf("#");
+            else
+                printf(" ");
+        }
+        printf(" ");
+        for()
+        printf("\n");
+    }
 }
