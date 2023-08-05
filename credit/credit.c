966348936538b3d15a0affff1dd34925b578f332
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-08-05 03:54:25 +0000

    Sat, Aug 5, 2023, 9:24 AM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -3,15 +3,20 @@
 
 int main(void)
 {
-    int n;
+    long n;
     do
     {
         n = get_long("Number: ");
     }
     while (n<0);
-    
+    int count = 0;
     for(i = n; n > 0; n/=10)
     {
-
+        int temp = n % 10;
+        if(count%2 != 0)
+        {
+            temp*=2;
+        }
+        
     }
 }
