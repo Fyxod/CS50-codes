595509c707b72b83fa3e248d2be04846903c0f5e
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-08-05 04:01:55 +0000

    Sat, Aug 5, 2023, 9:31 AM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -10,13 +10,36 @@ int main(void)
     }
     while (n<0);
     int count = 0;
-    for(i = n; n > 0; n/=10)
+    int sum = 0;
+        for(int i = n; n > 0; n/=10)
     {
         int temp = n % 10;
         if(count%2 != 0)
         {
             temp*=2;
         }
-        
+        int temp2 = SumOfDigits(temp);
+        sum= sum + temp2;
+        count++;
     }
+
+    if(sum%10==0)
+    {
+        printf("Visa\n");
+    }
+    else
+    {
+        printf("invalid\n")
+    }
+}
+
+int SumOfDigits(int h)
+{
+    int sum = 0;
+    for(int i = h; i>0; i/=10)
+    {
+        int temp = i%10;
+        sum+=temp;
+    }
+    return sum;
 }
