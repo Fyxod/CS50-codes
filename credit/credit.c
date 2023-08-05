6bc7e858fc72e7b4aa54f5585d6b67367281c8b5
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-08-05 15:01:35 +0000

    Sat, Aug 5, 2023, 8:31 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -1,5 +1,5 @@
-#include <math.h>
 #include <cs50.h>
+#include <math.h>
 #include <stdio.h>
 
 int SumOfDigits(int h);
@@ -10,53 +10,51 @@ int main(void)
     {
         n = get_long("Number: ");
     }
-    while (n<0);
+    while (n < 0);
     int count = 0;
     int sum = 0;
-        for(long i = n; i > 0; i/=10)
+    for (long i = n; i > 0; i /= 10)
     {
         int temp = i % 10;
-        if(count%2 != 0)
+        if (count % 2 != 0)
         {
-            temp*=2;
+            temp *= 2;
         }
         int temp2 = SumOfDigits(temp);
-        sum= sum + temp2;
+        sum = sum + temp2;
         count++;
     }
-    if(sum%10==0 && (int)(n/pow(10,(count-1)))==4 && (count==13||count==16))
+    if (sum % 10 == 0 && (int) (n / pow(10, (count - 1))) == 4 && (count == 13 || count == 16))
     {
         printf("VISA\n");
     }
     else
 
-        if(sum%10==0 && ((int)(n/pow(10,(count-2)))==34  || (int)(n/pow(10,(count-2)))==37) && count==15)
-        {
-            printf("AMEX\n");
-        }
+        if (sum % 10 == 0 && ((int) (n / pow(10, (count - 2))) == 34 || (int) (n / pow(10, (count - 2))) == 37) && count == 15)
+    {
+        printf("AMEX\n");
+    }
 
     else
 
-        if(sum%10==0 && ((int)(n/pow(10,(count-2)))<=55 && (int)(n/pow(10,(count-2)))>50) && count==16)
-        {
-            printf("MASTERCARD\n");
-        }
+        if (sum % 10 == 0 && ((int) (n / pow(10, (count - 2))) <= 55 && (int) (n / pow(10, (count - 2))) > 50) && count == 16)
+    {
+        printf("MASTERCARD\n");
+    }
 
     else
     {
         printf("INVALID\n");
     }
-
-
 }
 
 int SumOfDigits(int h)
 {
     int sum = 0;
-    for(int i = h; i>0; i/=10)
+    for (int i = h; i > 0; i /= 10)
     {
-        int temp = i%10;
-        sum+=temp;
+        int temp = i % 10;
+        sum += temp;
     }
     return sum;
 }
