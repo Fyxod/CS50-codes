6f27170d178e77569740e61f58e12466064b32a4
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-08-20 15:25:34 +0000

    Sun, Aug 20, 2023, 8:55 PM +05:30

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -13,14 +13,16 @@ int main(int argc, string argv[])
     if(argc == 2)
     {
 
-
-    string new = replace(argv[1]);
-
-    printf
+        string new = replace(argv[1]);
+        printf("%s", new);
     }
     else
     {
-        printf("You didn't type anything after exe")
+        printf("You didn't type anything after exe");
+        return 1;
     }
 }
 string replace(string t)
+{
+    
+}
