42c33330154f388e6c5b51a326bcaa972174e768
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-08-20 15:58:20 +0000

    Sun, Aug 20, 2023, 9:28 PM +05:30

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -11,7 +11,7 @@ string replace(string t);
 
 int main(int argc, string argv[])
 {
-    if(argc == 2)
+    if (argc == 2)
     {
 
         string new = replace(argv[1]);
@@ -28,25 +28,23 @@ string replace(string t)
 {
     string ret = "";
     int len = strlen(t);
-    for(int i = 0; i < len; i++)
+    for (int i = 0; i < len; i++)
     {
-        switch(t[i])
+        switch (t[i])
         {
             case 'a':
-                t[i]= '6';
+                t[i] = '6';
                 break;
             case 'e':
-                t[i]= '3';
+                t[i] = '3';
                 break;
             case 'i':
-                t[i]= '1';
+                t[i] = '1';
                 break;
             case 'o':
-                t[i]= '0';
+                t[i] = '0';
                 break;
         }
-
     }
     return t;
 }
-
