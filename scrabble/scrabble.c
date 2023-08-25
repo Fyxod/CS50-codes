c59c705092c4781c8e3c15d83f20dfd7c69e81a9
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-08-25 15:30:50 +0000

    Fri, Aug 25, 2023, 9:00 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -44,11 +44,13 @@ int compute_score(string word)
     {
         if(word[i] >= 'a' && word[i] <= 'z')
         {
+            printf("%s", word[i]);
             printf("%d\n", word[i] - 65);
             score = score + POINTS[(word[i] - 65)];
         }
         if(word[i] >= 'A' && word[i] <= 'Z')
         {
+            printf("%s", word[i]);
             printf("%d\n", word[i] - 97);
             score = score + POINTS[(word[i] - 97)];
         }
