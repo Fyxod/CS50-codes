2a26636b2a4315b9981fa750aacfa1bd7c45ebce
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-08-28 11:41:27 +0000

    Mon, Aug 28, 2023, 5:11 PM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -3,5 +3,11 @@
 
 int main(void)
 {
-    string str = get_string("Input a sentence)
+    string str = get_string("Text: ");
+    int letters = count_letters(str);
+    int words = count_words(str);
+    int sentences = count_sent(str);
+    double L = (letters/words) * 100;
+    double S = (sentences/words) * 100;
+    
 }
