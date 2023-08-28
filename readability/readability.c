f646b635989a1ecf2f1887976e37e5f550cb790b
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-08-28 18:24:54 +0000

    Mon, Aug 28, 2023, 11:54 PM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -27,7 +27,7 @@ int main(void)
         level = abs + 1;
     }
 
-    printf("Grade %i %lf\n", level, index);
+    printf("Grade %i %lf %lf %lf \n", level, index, L, S);
 }
 
 int count_letters(string s)
