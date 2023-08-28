aab5b5f6cedbd5ff802c419ea21c6c35b528fb49
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-08-28 18:12:44 +0000

    Mon, Aug 28, 2023, 11:42 PM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -43,7 +43,12 @@ int count_words(string s)
 {
     int len = strlen(s);
     int count = 0;
-    for(int i = 0; i < len; i++)
+    for(int i = 0; i < len-1; i++)
     {
-        if(
+        if(s[i] == ' ' && s[i+1] != ' ')
+        {
+            count++;
+        }
+    }
+    return count+1;
 }
