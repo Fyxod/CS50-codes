861067e7f5dc0225a6931c6d0d7d6b1c5089932a
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-09-06 11:46:15 +0000

    Wed, Sep 6, 2023, 5:16 PM +05:30

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,5 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
@@ -25,6 +27,12 @@ int main(int argc, string argv[])
 
 bool iskey(string key)
 {
-    int k = atoi(key);
-    
+    int len = strlen(key);
+    for(int i = 0; i < len; i++)
+    {
+        if(key[i] >= '0' && key[i] <= '9')
+        {
+            
+        }
+    }
 }
