32241bb0ede046a0355eaa283828060a296121e6
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-09-08 10:00:28 +0000

    Fri, Sep 8, 2023, 3:30 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,5 +3,14 @@
 
 int main(int argc, string argv[])
 {
-    if(argc)
+    if(argc!=2)
+    {
+        printf("Usage: ./substitution key");
+        return 1
+    }
+    else if(keycheck(argv[1]))
+    {
+        
+    }
+
 }
