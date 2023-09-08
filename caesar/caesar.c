7fd4ccfe362e67daba70b9f9cb9c9565b3cc5fe2
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-09-08 09:55:01 +0000

    Fri, Sep 8, 2023, 3:25 PM +05:30

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,8 +1,8 @@
 #include <cs50.h>
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 bool iskey(string key);
 string changex(string str, int key);
@@ -10,15 +10,14 @@ string changex(string str, int key);
 int main(int argc, string argv[])
 {
     string new;
-    printf("%i", argc);
-    if(argc != 2)
+    if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
     else
     {
-        if(iskey(argv[1]))
+        if (iskey(argv[1]))
         {
             new = changex((get_string("Plaintext:  ")), atoi(argv[1]));
             printf("ciphertext: %s\n", new);
@@ -26,6 +25,7 @@ int main(int argc, string argv[])
         else
         {
             printf("Usage: ./caesar key\n");
+            return 1;
         }
     }
 }
@@ -34,19 +34,18 @@ bool iskey(string key)
 {
     int len = strlen(key);
     int count = 0;
-    for(int i = 0; i < len; i++)
+    for (int i = 0; i < len; i++)
     {
-        if(!(key[i] >= '0' && key[i] <= '9'))
+        if (!(key[i] >= '0' && key[i] <= '9'))
         {
             count++;
         }
     }
-    printf("%i\n", count);
-    if(count > 0)
+    if (count > 0)
     {
         return 0;
     }
-    else if(atoi(key) >= 0)
+    else if (atoi(key) >= 0)
     {
         return 1;
     }
@@ -59,25 +58,37 @@ bool iskey(string key)
 string changex(string str, int key)
 {
     int len = strlen(str);
-    if(key > 26)
+    if (key > 26)
     {
         key = key % 26;
     }
-    for(int i = 0; i < len; i++)
+    for (int i = 0; i < len; i++)
     {
-        if(str[i] >='a' && str[i] <= 'z')
+        if (str[i] >= 'a' && str[i] <= 'z')
         {
-            if((str[i] + key) > 'z')
+            if ((str[i] + key) > 'z')
             {
-                key = key - ('z' - str[i]);
-                printf("%i ", key);
-                str[i] = 'z' + key;
+                int keytemp = key;
+                keytemp = keytemp - ('z' - str[i]);
+                str[i] = 'a' + (keytemp - 1);
             }
             else
             {
                 str[i] += key;
             }
-
+        }
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            if ((str[i] + key) > 'Z')
+            {
+                int keytemp = key;
+                keytemp = keytemp - ('Z' - str[i]);
+                str[i] = 'A' + (keytemp - 1);
+            }
+            else
+            {
+                str[i] += key;
+            }
         }
     }
     return str;
