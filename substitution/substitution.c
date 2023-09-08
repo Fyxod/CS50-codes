7eac4dce4adc07964fc1b89be2bbc6f6beb27afe
Fyxod <140262636+Fyxod@users.noreply.github.com>
2023-09-08 10:03:44 +0000

    Fri, Sep 8, 2023, 3:33 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,14 +3,21 @@
 
 int main(int argc, string argv[])
 {
+    string new;
     if(argc!=2)
     {
         printf("Usage: ./substitution key");
         return 1
     }
-    else if(keycheck(argv[1]))
+    else
     {
-        
-    }
+        if(keycheck(argv[1]))
+        {
+            new = change(get_string("plaintext:  "), argv[1])
+        }
+        else
+        {
+            return 1
+        }
 
 }
